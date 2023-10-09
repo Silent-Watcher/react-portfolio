98f3d4c53e29445f9f601b1618819f8c270e361d
Ali.t.nazari <alitabatabaee20@gmail.com>
2023-10-09 16:59:36 +0330

    intial commit & add header section

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import './Button.css';
+
+import PropTypes from 'prop-types';
+
+export default function Button({ link, name }) {
+  return <a className='btn' href={link}>{name}</a>;
+}
+
+Button.prototype = {
+  link: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
