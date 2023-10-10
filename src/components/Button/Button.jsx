6b70bdafdcf505a20fa0b69f3b1ee945a5a4bc27
Ali.t.nazari <alitabatabaee20@gmail.com>
2023-10-10 19:38:26 +0330

    fix(.): add semi rule to files

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,11 +3,14 @@ import './Button.css';
 import PropTypes from 'prop-types';
 
 export default function Button({ link, name }) {
-  return <a className='btn' href={link}>{name}</a>;
+	return (
+		<a className="btn" href={link}>
+			{name}
+		</a>
+	);
 }
 
 Button.prototype = {
-  link: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
+	link: PropTypes.string.isRequired,
+	name: PropTypes.string.isRequired,
 };
-
