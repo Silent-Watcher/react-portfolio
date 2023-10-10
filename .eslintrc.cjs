7add8f52d332385ac05e572f79d9401a4160ae37
Ali.t.nazari <alitabatabaee20@gmail.com>
2023-10-10 19:00:37 +0330

    feat: add alexjs, prettier, husky and lint-staged with commitizen

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,28 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'react/prop-types':'warn'
-  },
-}
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'plugin:@cspell/recommended',
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+		'prettier',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+	settings: { react: { version: '18.2' } },
+	plugins: ['react-refresh','prettier'],
+	rules: {
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+		'react/prop-types': 'warn',
+		semi: ['error', 'always'],
+		eqeqeq: 'warn',
+		camelcase: 'error',
+		'no-console': 'warn',
+		'prettier/prettier': 'error',
+	},
+};
